8919f75f5843ff53e0e90c5d4efb3bf049e3f98e
jdsolanki0001 <jdsolanki0001@gmail.com>
2021-03-02 15:44:46 +0530

    eslint + prettier configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,29 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/html-self-closing': 'off',
+
+    semi: ['error', 'never'],
+
+    'arrow-parens': ['error', 'as-needed'],
+
+    'lines-around-comment': [
+      'error',
+      {
+        beforeBlockComment: true,
+        beforeLineComment: true,
+      },
+    ],
+
+    'newline-before-return': 'error',
+
+    'import/newline-after-import': ['error', { count: 1 }],
   },
-};
+}
